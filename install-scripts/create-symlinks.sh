7703bdc0db79532a39fb560cc5e999fd0d40934e
Jimmy Cui <jimmy.cui@sada.com>
2021-02-08 15:40:11 -0500

    bug

--- install-scripts/create-symlinks.sh
+++ install-scripts/create-symlinks.sh
@@ -54,8 +54,9 @@ add_default_dot_files () {
 
 add_vim_plug_in () {
     if [ -f $HOME/.vimrc ] ; then
-    mkdir -p $HOME/.vim
-    cp $dotfiles_dir/vim/* $HOME/.vim
+        mkdir -p $HOME/.vim
+        cp $dotfiles_dir/vim/* $HOME/.vim
+    fi
 }
 
 #==============
